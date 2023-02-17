5daa02ff5b22349ab11065ad8488493afea2e27c
frenesi-code <paulo.guitarrista@gmail.com>
2023-02-16 20:42:52 -0800

    step 2: completed

--- js/script.js
+++ js/script.js
@@ -8,5 +8,11 @@ let summerMonth
 let MyLibraryFunction
 
 // STEP 2
+// Give me an example of a numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression.
+
+let numberValue = 42;
+let stringValue = "Hello, world!";
+let boolValue = true;
+let nullValue = null;
 
 // STEP 3
