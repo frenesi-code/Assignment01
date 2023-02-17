31f57d3297f7ae87a528b275b42eb44ed3171949
frenesi-code <paulo.guitarrista@gmail.com>
2023-02-16 20:58:06 -0800

    step 4: completed

--- js/script.js
+++ js/script.js
@@ -23,3 +23,15 @@ const result = ((a, b) => {
   return sum * diff;
 })(2, 4);
 
+// STEP 4
+// Declare (but do not assign) 9 variables for the following identifiers: First Name, Last Name, Address, City, State, Zip Code, Your Age, Referral Source, May We Contact You. Use Camel Casing and Hungarian Notation when naming your identifiers.
+
+let strFirstName;
+let strLastName;
+let strAddress;
+let strCity;
+let strState; 
+let nZipCode;
+let iYourAge;
+let strReferralSource;
+let strMayWeContactYou;
