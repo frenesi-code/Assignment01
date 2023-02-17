6cbda10d7c106538ca3673eb5ac6fa9eae087628
Paulo Faulstich <paulo.faulstich@gmail.com>
2023-02-16 19:55:05 -0800

    step 1: completed

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// STEP 1
+// Convert the following highlighted identifiers to Camel Case notation
+
+let someMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let MyLibraryFunction
+
+// STEP 2
+
+// STEP 3
